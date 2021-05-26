24b4b7f0cf3333d069425299d9877da1c0731d4d
Dark7lord <budda185@mail.ru>
2021-05-26 06:31:06 +0500

    В целом сверстал версию для планшета

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,1 +1,12 @@
-console.log('hi');
+'use strict';
+// Header
+
+const header = document.querySelector('.header');
+console.log(header);
+const headerToggle = document.querySelector('.header__toggle');
+
+header.classList.remove('header--no-js');
+
+headerToggle.addEventListener('click', () => {
+  header.classList.toggle('header--opened');
+});
