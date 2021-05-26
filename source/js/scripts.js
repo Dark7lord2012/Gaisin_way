c5f42cd2df74c7db03b68dbf5df06eec918503e8
Dark7lord <budda185@mail.ru>
2021-05-26 14:22:54 +0500

    Не добитый вовремя код я закомментировал, наступил час дедлайна, время на ноуте странно отстает, от греха подольше запушу и закомичу в гитхаб

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,7 +2,6 @@
 // Header
 
 const header = document.querySelector('.header');
-console.log(header);
 const headerToggle = document.querySelector('.header__toggle');
 
 header.classList.remove('header--no-js');
@@ -10,3 +9,13 @@ header.classList.remove('header--no-js');
 headerToggle.addEventListener('click', () => {
   header.classList.toggle('header--opened');
 });
+
+// const btnsPriceLink = document.querySelectorAll('.price__link');
+// const popup = document.querySelector('.popup');
+
+// for (const btnPriceLink of btnsPriceLink) {
+//   btnPriceLink.addEventListener('click', () => {
+//     popup.classList.add('popup--opened');
+//     console.log('hi');
+//   });
+// }
